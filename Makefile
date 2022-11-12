4d416e6facb9101be7d2aede5988b4e9999e51d4
YuliaMick <julia3694510@gmail.com>
2022-11-12 20:08:49 +0300

    step 3

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
-develop:
-	npx webpack serve
-
-install:
+install: 
 	npm ci
+
+dev:
+	npx webpack serve
 
 build:
 	rm -rf dist
 	NODE_ENV=production npx webpack
 
 lint:
-	npx eslint 
+	npx eslint .
--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,65 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-import path from 'path';
+import path, { dirname } from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const isProduction = process.env.NODE_ENV == 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
 
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run post css actions
+        }, {
+          loader: 'sass-loader', // compiles Sass to CSS
+        }],
+      },
+
+    // Add your rules for custom modules here
+    // Learn more about loaders from https://webpack.js.org/loaders/
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-            {
-                test: /\.(scss)$/,
-                use: [{
-                  loader: 'style-loader', // inject CSS to page
-                }, {
-                  loader: 'css-loader', // translates CSS into CommonJS modules
-                }, {
-                  loader: 'postcss-loader', // Run post css actions
-                }, {
-                  loader: 'sass-loader' // compiles Sass to CSS
-                }]
-              },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
-        ],
-    },
+  },
 };
 
 export default () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
+  if (isProduction) {
+    config.mode = 'production';
+  } else {
+    config.mode = 'development';
+  }
+  return config;
 };
